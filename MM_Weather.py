9f2d6bce37008341663fab83d28a554a124a5ebb
Michelle Madiba <130059963+Michelle-Madiba@users.noreply.github.com>
2023-09-22 19:59:38 +0200

    Update MM_Weather.py

--- MM_Weather.py
+++ MM_Weather.py
@@ -1,62 +1,42 @@
 import streamlit as st
 import requests
-import os
-import json
 
-
-# Replace 'your_api_key_here' with your actual OpenWeatherMap API key
-os.environ['MM_open_weather'] = 'eef94865433aba6d8689c10961915c02'
 # Set the page title
 st.set_page_config(
-    page_title="Weather Now",
-    page_icon=":partly_sunny:",  # You can use emojis as icons
-    layout="centered",  # Adjust the layout
-    initial_sidebar_state="expanded"  # Expand the sidebar by default
+    page_title="OpenWeather API Data",
+    page_icon=":partly_sunny:",
+    layout="wide"
 )
 
-# Set Streamlit title
-st.title("Weather NOW")
+# Streamlit title and description
+st.title("OpenWeather API Data")
+st.write("This app fetches and displays weather data from the OpenWeather API.")
 
-# Retrieve the OpenWeatherMap API key from the environment variable
-openweather_api = os.environ.get('MM_open_weather')
-if not openweather_api:
-    st.warning("Please set the 'MM_open_weather' environment variable with your API key.")
-else:
-    # Ask the user to input the city name
-    location = st.text_input('Enter city name:')
+# User input for city name
+city_name = st.text_input('Enter city name:', 'New York')  # Default city name is "New York"
 
-    if st.button('Get Weather'):
-        # Set the base URL for the OpenWeatherMap API
-        base_url = 'http://api.openweathermap.org/geo/1.0/direct?q='
-        #{city name},{state code},{country code}{limit}&appid={API key}
+# API configuration
+api_key = 'eef94865433aba6d8689c10961915c02'  # Replace with your OpenWeather API key
+base_url = 'http://api.openweathermap.org/data/2.5/weather'
 
-        # Set the API key
-        api_key = openweather_api
+# Display weather data when the user clicks the "Get Weather" button
+if st.button('Get Weather'):
+    # API request parameters
+    params = {'q': city_name, 'appid': api_key, 'units': 'metric'}  # 'units' for temperature in Celsius
 
-        # Create the URL for the API request with 'q' parameter for the city name
-        url = base_url  + location + '&limit=3'+'&appid=' + api_key
+    # Make the API request
+    response = requests.get(base_url, params=params)
 
-        # Send the request to the API
-        response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        st.subheader(f"Weather in {city_name}")
+        st.write(f"Temperature: {data['main']['temp']}°C")
+        st.write(f"Description: {data['weather'][0]['description'].capitalize()}")
+        st.write(f"Humidity: {data['main']['humidity']}%")
+        st.write(f"Pressure: {data['main']['pressure']} hPa")
+    else:
+        st.error(f"Error: Unable to retrieve weather data. Status code: {response.status_code}")
 
-        if response.status_code == 200:
-            # Parse the JSON response
-            data = json.loads(response.text)
-            # Create a DataFrame to store weather data
-            weather_data = {
-                "Attribute": ["City", "Description", "Temperature (°C)", "Humidity (%)"],
-                "Value": [data['name'], data['weather'][0]['description'], data['main']['temp'], data['main']['humidity']]
-            }
-
-            df = pd.DataFrame(weather_data)
-
-            # Display weather icon
-            weather_icon = data['weather'][0]['icon']
-            st_icon(weather_icon, width=100)
-
-            # Display the DataFrame using pandas
-            st.table(df)
-        else:
-            st.error(f"Error: Unable to retrieve weather data. Status code: {response.status_code}")
-
+# Display a footer with attribution to OpenWeatherMap
+st.markdown("Data provided by [OpenWeatherMap](https://openweathermap.org/).")
 
